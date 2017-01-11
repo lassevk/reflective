17760e4bc213bd1d032894d7fbba03fcd49808ea
Lasse V. Karlsen <lasse@vkarlsen.no>
2017-01-11 11:11:21 +0100

    Removed unnecessary files. Added test to list all missing opcode implementations.

---
+++ Reflective.Tests/MissingImplementation.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+// ReSharper disable PossibleNullReferenceException
+
+namespace Reflective.Tests
+{
+    [TestFixture]
+    public class MissingImplementation
+    {
+        public static IEnumerable<TestCaseData> TestCases()
+        {
+            foreach (var value in typeof(OpCodes).GetFields())
+                yield return new TestCaseData(value.Name).SetName(value.Name);
+        }
+
+        [Test]
+        [TestCaseSource(nameof(TestCases))]
+        public void ImplementationExists(string opcode)
+        {
+            bool implementationExists = typeof(ILGeneratorFluent).GetMethod(opcode) != null;
+            Assert.That(implementationExists);
+        }
+    }
+}
