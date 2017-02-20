027a7ad376ed46b7a17f6c3aa027e71f48c10963
Lasse V. Karlsen <lasse@vkarlsen.no>
2017-02-20 09:45:11 +0100

    Added Xor opcode.

---
+++ Reflective.Tests/OpCodeTests.Xor.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Reflective.Tests
+{
+    public partial class OpCodeTests
+    {
+        [Test]
+        public void Xor() => Test(il => il.Xor(), 0x61);
+    }
+}
---
+++ Reflective/ILGeneratorFluent.Xor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Reflection.Emit;
+
+namespace Reflective
+{
+    public partial struct ILGeneratorFluent
+    {
+        /// <summary>
+        /// Computes the bitwise XOR of the top two values on the evaluation stack, pushing the result onto the evaluation stack.
+        /// </summary>
+        /// <see href="https://msdn.microsoft.com/en-us/library/system.reflection.emit.opcodes.xor(v=vs.110).aspx"/>
+        public ILGeneratorFluent Xor() => Emit(OpCodes.Xor);
+    }
+}
